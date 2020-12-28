868ed61ebcc8d473c30ff5b992b3d07d27564592
Debagnik Roy <debagnikroy@gmail.com>
2020-12-28 15:27:43 +0530

    first commit

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ const port = process.env.PORT || 8001;
 
 
 if (process.env.NODE_ENV === "production"){
-    app.use(express.static("build"));
+    var distDir = __dirname + "/build/";
+    app.use(express.static("distDir"));
     app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname,  "build", "index.html"));
     });
