e108514187d6aa0b7dfeff6d1efce7c276ee67dc
Debagnik Roy <debagnikroy@gmail.com>
2020-12-28 15:21:40 +0530

    first commit

--- app.js
+++ app.js
@@ -34,9 +34,14 @@ app.use('/api', bookRoutes);
 
 const port = process.env.PORT || 8001;
 
-if(process.env.NODE_ENV=== 'production'){
-    app.use(express.static('client/build'));
-}
+
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
