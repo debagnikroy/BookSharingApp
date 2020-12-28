872aaa7f4fd69a69dba5dc2de6c14d7352ea2122
Debagnik Roy <debagnikroy@gmail.com>
2020-12-28 17:11:52 +0530

    added names

--- app.js
+++ app.js
@@ -35,13 +35,7 @@ app.use('/api', bookRoutes);
 
 const port = process.env.PORT || 8001;
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+
 
 if (process.env.NODE_ENV === "production"){
     app.use(express.static('client/build'));
